fe112fe83c9f7faaf6a4da05429e744ba47ebf2b
amanagarwal0602 <amanagarwal0602@gmail.com>
2025-07-23 23:18:01 +0530

    Result Analysis Basic Syntax V0

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+class even odd
+{
+public static void main(String[] args)
+{
+int n;
+for (n=0;n<=100;n++)
+if(n%2==1)
+{
+System.out.println(n + " is odd");
+}
+else
+{
+System.out.println(n+" is even");
+}
+}
+}
